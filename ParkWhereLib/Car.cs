64d5a9643aa9b78a6aae0e51b99b9f522e368115
Martin Frost Lauridsen <mal021@edu.zealand.dk>
2025-11-24 10:24:01 +0100

    Nye klasser samt controller

--- ParkWhereLib/Car.cs
+++ ParkWhereLib/Car.cs
@@ -1,6 +1,6 @@
 ﻿namespace ParkWhereLib
 {
-    public class Class1
+    public class Car
     {
 
     }
---
+++ ParkWhereRest/Controllers/ParkWhereController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParkWhereRest.Controllers
+{
+    public class ParkWhereController : Controller
+    {
+        // GET: ParkWhereController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ParkWhereController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ParkWhereController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParkWhereController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ParkWhereController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ParkWhereController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ParkWhereController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ParkWhereController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
