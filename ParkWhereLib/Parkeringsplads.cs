3ada23275f55b58d66a569676dd7e42f3f18d2fd
Martin Frost Lauridsen <mal021@edu.zealand.dk>
2025-11-26 09:31:17 +0100

    cjlwel

---
+++ ParkWhereLib/Parkeringsplads.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkWhereLib
+{
+    public class Parkeringsplads
+    {
+        public int Id { get; set; }
+
+        public int Parkeringspladser { get; set; }
+
+
+    }
+}
