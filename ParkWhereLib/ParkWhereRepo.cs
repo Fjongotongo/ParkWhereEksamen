7fb46483a102ae81d91c1ebe162f0b891b2c096b
Nikolaj04 <felsdd04@gmail.com>
2025-11-27 15:54:32 +0100

    calculate parking spaces left methods for when cars is driving into and out of parking lot

--- ParkWhereLib/ParkWhereRepo.cs
+++ ParkWhereLib/ParkWhereRepo.cs
@@ -25,7 +25,7 @@ namespace ParkWhereLib
 
             _parkingLots = new List<ParkingLot>()
             {
-                new ParkingLot { Id = _nextParkingLotId++, Name = "P-Syd", ParkingSpaces = 75 }
+                new ParkingLot { Id = _nextParkingLotId++, Name = "P-Syd", ParkingSpaces = 75, CarsParked = 73 }
             };
         }
 
@@ -84,9 +84,44 @@ namespace ParkWhereLib
             return car;
         }
 
-        public int? AddCarToParkingLotWhenEmptySpaces(int carId, int parkingLotId)
+        /// <summary>
+        /// Calculates the amount of parking spaces left when a car has driven into the lot.
+        /// </summary>
+        /// <param name="parkingLotId"></param>
+        /// <returns>
+        /// Returns the amount of parkingspaces left after new car is parked.
+        /// </returns>
+        public int CalculateParkingSpacesWhenCarIsDrivingIntoParkingLot(int parkingLotId)
         {
-            return null;
+            ParkingLot? parkingToRemoveSpace = _parkingLots.FirstOrDefault(p => p.Id == parkingLotId);
+            int parkingSpacesLeft = 0;
+
+            if (parkingToRemoveSpace != null)
+            {
+                parkingToRemoveSpace.CarsParked += 1;
+                parkingSpacesLeft = parkingToRemoveSpace.ParkingSpaces - parkingToRemoveSpace.CarsParked;
+            }
+            return parkingSpacesLeft;
+        }
+
+        /// <summary>
+        /// Calculates the amount of parking spaces left when a car has driven out of the lot
+        /// </summary>
+        /// <param name="parkingLotId"></param>
+        /// <returns>
+        /// Parking spaces left after car has left lot.
+        /// </returns>
+        public int CalculateParkingSpacesWhenCarIsDrivingOutOfParkingLot(int parkingLotId)
+        {
+            ParkingLot? parkingToAddSpace = _parkingLots.FirstOrDefault(p => p.Id == parkingLotId);
+            int parkingSpacesLeft = 0;
+
+            if (parkingToAddSpace != null)
+            {
+                parkingToAddSpace.CarsParked -= 1;
+                parkingSpacesLeft = parkingToAddSpace.ParkingSpaces - parkingToAddSpace.CarsParked;
+            }
+            return parkingSpacesLeft;
         }
     }
 }
