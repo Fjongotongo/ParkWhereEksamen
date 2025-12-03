846274a3eb0ff1d9226b52170c22dea18790adf4
Ozzbuzzz69 <oscarfussings@outlook.dk>
2025-12-03 11:44:37 +0100

    Rest

--- ParkWhereRestTests/Controllers/ParkWhereControllerTests.cs
+++ ParkWhereRestTests/Controllers/ParkWhereControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ParkWhereLib;
 using ParkWhereRest.Controllers;
@@ -16,11 +18,34 @@ namespace ParkWhereRest.Controllers.Tests
         private ParkWhereController _controller;
         private HttpClient _httpClient;
 
+        private ParkingLot _parkingLot;
+        private ParkWhereController _parkWhereController;
 
         [TestInitialize]
         public void TestInitialize()
         {
+            _parkingLot = new ParkingLot()
+            {
+                ParkingSpaces = 100,
+                CarsParked = 1,
+                AvailableSpaces = 99
+            };
+            _parkWhereController = new ParkWhereController(_parkingLot);
         }
+
+        [TestMethod()]
+        public void Test_Get_Available_Parking_Spots()
+        {
+            int expected = 98;
+
+            ParkWhereController.PlateDto plateDto = new ParkWhereController.PlateDto
+            {
+                Plate = "AB12345",
+                time = DateTime.Now
+            };
+            ActionResult<int> result = _parkWhereController.Available_Parking_Spots(plateDto);
+            Assert.AreEqual(expected, (int)result.Value);
+        } 
 
         //[TestMethod()]
         //public void GetAllTest()
