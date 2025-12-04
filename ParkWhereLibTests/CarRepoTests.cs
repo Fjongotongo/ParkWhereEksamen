666cf10b0cfa3d8bbfb5391824e995809b3897f3
Nikolaj04 <felsdd04@gmail.com>
2025-12-04 11:36:50 +0100

    addcar test and implementation

---
+++ ParkWhereLibTests/CarRepoTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkWhereLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkWhereLib.Tests
+{
+    [TestClass()]
+    public class CarRepoTests
+    {
+        private CarRepo _carRepo;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _carRepo = new CarRepo();
+        }
+
+        [TestMethod()]
+        public void CreateCarTest()
+        {
+            Car car = new Car
+            {
+                Brand = "Toyota",
+                Model = "Corolla",
+                Fueltype = "Gas"
+            };
+
+            int expectedId = 1;
+
+            Car carAdded = _carRepo.AddCarToRepo(car);
+
+            Assert.AreEqual(expectedId, carAdded.Id);
+            Assert.AreEqual(car.Brand, carAdded.Brand);
+            Assert.AreEqual(car.Model, carAdded.Model);
+            Assert.AreEqual(car.Fueltype, carAdded.Fueltype);
+        }
+    }
+}
