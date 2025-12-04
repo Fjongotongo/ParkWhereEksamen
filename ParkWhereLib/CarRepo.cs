eccd18329aa5df345a64f9907e6c04f5bfc6a5d6
Nikolaj04 <felsdd04@gmail.com>
2025-12-04 11:15:12 +0100

    carrepo

---
+++ ParkWhereLib/CarRepo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkWhereLib
+{
+    public class CarRepo
+    {
+        public List<Car> Cars = new List<Car>();
+
+        public Car AddCar(Car car)
+        {
+            return null;
+        }
+    }
+}
