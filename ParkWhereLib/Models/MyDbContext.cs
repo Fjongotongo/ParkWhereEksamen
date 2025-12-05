f38b1001bfaa3814cf7b999124dfaaa1ec920414
Mads <Mads@DESKTOP-HJFDOR1>
2025-12-05 09:04:14 +0100

    DB opsætning

---
+++ ParkWhereLib/Models/MyDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace ParkWhereLib.Models;
+
+public partial class MyDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options, IConfiguration configuration)
+            : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Car> Cars { get; set; }
+
+    public DbSet<ParkingEvent> ParkingEvents { get; set; }
+
+    public DbSet<ParkingLot> ParkingLots { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
